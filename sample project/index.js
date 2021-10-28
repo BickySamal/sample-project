4eb1332a3d7556abc8d7ef05ff8a04328d2052d0
BickySamal <34908641+BickySamal@users.noreply.github.com>
2021-10-28 14:23:26 +0530

    Add files via upload

---
+++ sample project/index.js
@@ -0,0 +1,9 @@
+const navlinks = document.getElementById('navlink');
+
+function showMenu(){
+    navlinks.style.right="0";
+    console.log("working");
+}
+function hideMenu(){
+    navlinks.style.right="-230px"
+}
